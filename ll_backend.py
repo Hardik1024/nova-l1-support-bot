a66bf4e462d2dec585cdbbbfb2f8c6393abeace0
Hardik1024 <hardik.aggarwal2410@gmail.com>
2026-08-31 23:40:46 +0530

    Fix Streamlit deployment

--- ll_backend.py
+++ ll_backend.py
@@ -4,6 +4,7 @@ import os
 import platform
 import psutil
 import requests
+import streamlit as st
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import SystemMessage,HumanMessage,AIMessage,ToolMessage
